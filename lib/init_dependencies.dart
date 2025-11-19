2daa63021ee67fe0a7d0728dd2eb5569ddef10e3
Aayush Adhikari <aayushxadhikari@gmail.com>
2025-11-19 21:51:37 +0545

    added the hive local storage

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -1,5 +1,7 @@
 import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:siren_app/core/common/cubits/app_user/app_user_cubit.dart';
 import 'package:siren_app/core/network/connection_checker.dart';
 import 'package:siren_app/core/secrets/app_secrets.dart';
@@ -10,6 +12,7 @@ import 'package:siren_app/features/auth/domain/usecases/current_user.dart';
 import 'package:siren_app/features/auth/domain/usecases/user_login.dart';
 import 'package:siren_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:siren_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:siren_app/features/blog/data/datasources/blog_local_data_source.dart';
 import 'package:siren_app/features/blog/data/datasources/blog_remote_data_source.dart';
 import 'package:siren_app/features/blog/data/repositories/blog_repository_impl.dart';
 import 'package:siren_app/features/blog/domain/repositories/blog_repository.dart';
@@ -17,6 +20,7 @@ import 'package:siren_app/features/blog/domain/usecases/get_all_blogs.dart';
 import 'package:siren_app/features/blog/domain/usecases/upload_blog.dart';
 import 'package:siren_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:uuid/uuid.dart';
 
 final serviceLocator = GetIt.instance;
 
@@ -27,8 +31,13 @@ Future<void> initDependencies() async {
     url: AppSecrets.supabaseUrl,
     anonKey: AppSecrets.supabaseAnnonKey,
   );
+  final dir = await getApplicationDocumentsDirectory();
+  Hive.init(dir.path);
 
   serviceLocator.registerLazySingleton(() => supabase.client);
+
+  serviceLocator.registerLazySingleton(() => Hive.openBox('blogs'));
+
   serviceLocator.registerFactory(() => InternetConnection());
 
   // core
@@ -68,8 +77,15 @@ void _initBlog() {
     ..registerFactory<BlogRemoteDataSource>(
       () => BlogRemoteDataSourceImpl(serviceLocator()),
     )
+    ..registerFactory<BlogLocalDataSource>(
+      () => BlogLocalDataSourceImpl(serviceLocator()),
+    )
     ..registerFactory<BlogRepository>(
-      () => BlogRepositoryImpl(serviceLocator()),
+      () => BlogRepositoryImpl(
+        serviceLocator(),
+        serviceLocator(),
+        serviceLocator(),
+      ),
     )
     ..registerFactory(() => UploadBlog(serviceLocator()))
     ..registerFactory(() => GetAllBlogs(serviceLocator()))
